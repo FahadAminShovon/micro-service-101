ae7ecdd6392d541ae5d7039f2823ee84c5372a02
FahadAminShovon <shovonfahad991@gmail.com>
2025-06-15 18:11:34 +0600

    :construction: add comments and posts api

--- apps/comments/src/index.ts
+++ apps/comments/src/index.ts
@@ -1,16 +1,46 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { randomBytes } from 'crypto';
 
 const app = new Hono();
+
+type TComment = {
+  id: string;
+  content: string;
+};
+
+type TCommentsByPostId = {
+  [key: string]: TComment[];
+};
+
+const commentsByPostId: TCommentsByPostId = {};
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
 
+app.get('/posts/:id/comments', (c) => {
+  const postId = c.req.param('id');
+  const comments = commentsByPostId[postId] ?? [];
+  return c.json(comments, 200);
+});
+
+app.post('/posts/:id/comments', async (c) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = await c.req.json();
+  const postId = c.req.param('id');
+
+  const comments = commentsByPostId[postId] ?? [];
+  commentsByPostId[postId] = comments;
+  comments.push({ id: commentId, content });
+
+  return c.json(comments, 201);
+});
+
 serve(
   {
     fetch: app.fetch,
-    port: 3001,
+    port: 4001,
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
--- apps/posts/src/index.ts
+++ apps/posts/src/index.ts
@@ -1,15 +1,42 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { randomBytes } from 'crypto';
 
-const app = new Hono()
+export const app = new Hono();
+
+type TPost = {
+  id: string;
+  title: string;
+};
+type TPosts = {
+  [key: string]: TPost;
+};
+const posts: TPosts = {};
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get('/posts', (c) => {
+  return c.json(posts, 200);
+});
+
+app.post('/posts', async (c) => {
+  const id = randomBytes(4).toString('hex');
+  const body = await c.req.json();
+  posts[id] = {
+    id,
+    title: body.title,
+  };
+  return c.json(posts[id], 201);
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 4000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
